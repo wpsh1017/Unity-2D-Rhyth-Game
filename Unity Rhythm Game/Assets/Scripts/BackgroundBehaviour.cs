4994b436efe8d9135cd2a6e118a100db4f91c859
zenobreaker <wpsh6369@naver.com>
2019-03-23 14:18:17 +0900

    에셋 추가 및 게임 화면 구성

---
+++ Unity Rhythm Game/Assets/Scripts/BackgroundBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundBehaviour : MonoBehaviour
+{
+    public GameObject gameBackground;
+    private SpriteRenderer gameBackgroundSpriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameBackgroundSpriteRenderer = gameBackground.GetComponent<SpriteRenderer>();
+        StartCoroutine(FadeOut(gameBackgroundSpriteRenderer, 0.005f));
+    }
+
+    IEnumerator FadeOut(SpriteRenderer spriteRenderer, float amount)
+    {
+        Color color = spriteRenderer.color;
+        while (color.a > 0.0f)
+        {
+            color.a -= amount;
+            spriteRenderer.color = color;
+            yield return new WaitForSeconds(amount);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
